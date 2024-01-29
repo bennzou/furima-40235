d34324d0f994cf3e60300eb94d22528c2076b481
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-01-29 17:21:02 +0900

    usersテーブル設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
 
-  def basic_auth
+  def configure_permitted_parameters
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
