b365b14b74b793e84e04b6d7f9c554b1776263e0
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-03 21:55:30 +0900

    ユーザー管理機能　修正　４

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
+   with_options presence: true do
 
-  validates :nickname, uniqueness: true, length: { maximum: 6 }
+   validates :nickname, uniqueness: true, length: { maximum: 6 }
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+   validates :last_name
+   validates :first_name
+   end
+   with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+   validates :last_name_kana
+   validates :first_name_kana
+   end
+   validates :birth_date
+
+   end
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
     
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  end
-  validates :birth_date, presence: true
-  end
 end
