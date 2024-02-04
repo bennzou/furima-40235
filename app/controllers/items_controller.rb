9146f14fd65df531879052a50648dae8b3edad30
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-04 19:32:46 +0900

    商品出品機能　Views編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
- def create
+  def new
+    @item = Item.new
+   end
+
+  def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -10,4 +14,4 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-end
+end 
