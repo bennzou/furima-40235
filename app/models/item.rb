12649120577cc990c5bb6e7853b805b416fd23cd
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-08 17:06:33 +0900

    テストコード入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :user
 has_one_attached :image
 
-
 belongs_to :category
 belongs_to :prefecture
 belongs_to :item_status
@@ -20,11 +19,17 @@ belongs_to :shipping_date
     validates :item_shipping_fee_status_id
     validates :item_sales_status_id 
     validates :item_prefecture_id
+    validates :item_scheduled_delivery_id 
     validates :item_price
-    validates :user_id
 
     validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-
+  with_options numericality: { other_than: 0 } do
+    validates :item_category_id
+    validates :item_prefecture_id
+    validates :item_sales_status_id 
+    validates :item_scheduled_delivery_id
+    validates :item_shipping_fee_status_id
+  end
 end
