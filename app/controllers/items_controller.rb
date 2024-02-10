c74e7e5bd467e513e4d9aea229594a3336d839a7
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-10 18:53:41 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,16 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
+
   def new
     @item = Item.new
   end
+
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
+
 
   def create
     @item = Item.new(item_params)
@@ -20,6 +27,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id,
                                  :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.all
   end
 
 end
