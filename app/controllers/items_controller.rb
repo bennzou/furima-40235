76c616796741bc52bc16d0ef098eda1c7d79214d
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-10 21:29:44 +0900

    show.index の入力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,9 @@ class ItemsController < ApplicationController
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
+  def show
+    @item = Item.find(params[:id])
+  end
 
 
 
