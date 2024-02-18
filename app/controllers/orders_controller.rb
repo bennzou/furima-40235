33f88d6ef9ddf36614568005451c16d8ab4c85f3
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-18 20:42:26 +0900

    商品購入機能　カード情報

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,10 +9,13 @@ class OrdersController < ApplicationController
     def create
       @order_form = OrderForm.new(order_params)
       if @order_form.valid?
+        pay_item
+        
          @order_form.save
         redirect_to root_path
       else
         render :index, status: :unprocessable_entity
+      
       end
     end
   
@@ -21,6 +24,17 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order_form).permit(:post_code, :item_prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     end
+
+    def pay_item
+      Payjp.api_key = "sk_test_687c2fb568e785bef9973258"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+        Payjp::Charge.create(
+          amount: @item.item_price,  # 商品の値段
+          card: order_params[:token],    # カードトークン
+          currency: 'jpy'                 # 通貨の種類（日本円）
+        )
+      )
+    end
+
   
     def set_item
       @item = Item.find(params[:item_id])
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,7 +1,6 @@
 class OrderForm
     include ActiveModel::Model
     attr_accessor :user_id, :item_id, :post_code, :item_prefecture_id, :city, :block, :building, :phone_number, :token
-
     with_options presence: true do
         validates :user_id
         validates :item_id
