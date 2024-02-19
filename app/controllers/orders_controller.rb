7a7b393b114c07a4fc8217d9ba6b42d25a7e06f3
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-19 12:01:30 +0900

    商品購入機能　提出

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,15 +10,14 @@ class OrdersController < ApplicationController
     def create
       @order_form = OrderForm.new(order_params)
       if @order_form.valid?
-        pay_item
-        
+         pay_item
          @order_form.save
-        redirect_to root_path
+         return redirect_to root_path
       else
-        render :index, status: :unprocessable_entity
-      
-      end
+        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+        render 'index', status: :unprocessable_entity
     end
+end
   
     private
   
@@ -26,17 +25,16 @@ class OrdersController < ApplicationController
       params.require(:order_form).permit(:post_code, :item_prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     end
 
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+
     def pay_item
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp.api_key =  ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
         Payjp::Charge.create(
           amount: @item.item_price,  # 商品の値段
           card: order_params[:token],    # カードトークン
           currency: 'jpy'                 # 通貨の種類（日本円）
         )
-    end
-
-  
-    def set_item
-      @item = Item.find(params[:item_id])
     end
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
+  const publicKey = gon.public_key;
+  const payjp = Payjp(publicKey); // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -10,24 +10,27 @@ const pay = () => {
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
 
-  const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
+  const form = document.getElementById('charge-form');
+  form.addEventListener('submit', (e) => {
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
+        // エラーが発生した場合の処理を追加
+        console.error(response.error.message);
       } else {
         const token = response.id;
-        const renderDom = document.getElementById("charge-form");
+        const renderDom = form; // form を使うように修正
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        renderDom.insertAdjacentHTML('beforeend', tokenObj);
       }
       numberElement.clear();
       expiryElement.clear();
       cvcElement.clear();
-      //document.getElementById("charge-form").submit();
+      // フォームの送信
+      form.submit();
     });
     e.preventDefault();
   });
 };
 
-window.addEventListener("turbo:load", pay);
-window.addEventListener("turbo:render", pay);
+window.addEventListener('turbo:load', pay);
+window.addEventListener('turbo:render', pay);
