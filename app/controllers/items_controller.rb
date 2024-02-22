f4eab626079ce49154a7fee9327e2766a2669e9c
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-02-22 15:42:14 +0900

    商品購入機能　修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,22 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show ]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+
 
   def new
     @item = Item.new
   end
 
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
 
+
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
@@ -20,19 +24,25 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
   end
-
   def destroy
+    
     if @item.user_id == current_user.id
       @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
-    redirect_to root_path
   end
 
+
   def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
+    if @item.valid?
+      @item.update(item_params)
+      redirect_to item_path(item_params)
     else
       render 'edit', status: :unprocessable_entity
     end
